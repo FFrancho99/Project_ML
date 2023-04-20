b5794904f3d5829e77433d76f91867a0087829bf
Florence Franchomme <flofranchomme@gmail.com.adress>
2023-04-20 18:47:43 +0200

    test2

--- Model/Generator.py
+++ Model/Generator.py
@@ -13,6 +13,7 @@ class Generator(nn.Module):
     def __init__(self):
         super(Generator,self).__init__()
 
+        # To change (here only to have structure)
         self.layer1 = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(4, 4), stride=2, padding=1),
             nn.ReLU(inplace=True)
