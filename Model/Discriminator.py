9659a25ea9bebbce321c91f29f6299d8f8ab63be
Robin <robin.b64@hotmail.com>
2023-04-28 01:50:20 +0200

    test commit

--- Model/Discriminator.py
+++ Model/Discriminator.py
@@ -13,24 +13,31 @@ class Discriminator(nn.Module):
     def __init__(self, input_channels):
         super().__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=input_channels, out_channels=6,
-                      kernel_size=(5, 5)),
+            nn.Conv2d(in_channels=input_channels, out_channels=6, kernel_size=(5, 5)),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-        )  # 6@14x14
+            nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        )#6@30x30
 
         self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=(3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        )#16@14x14
+
+        self.layer3 = nn.Sequential(
             nn.Conv2d(in_channels=6, out_channels=16,
                       kernel_size=(5, 5)),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-        )  # 16@5x5
-        self.layer3 = nn.Sequential(
+        )# 16@5x5
+
+        self.layer4 = nn.Sequential(
             nn.Flatten(),
             nn.Linear(16 * 5 * 5, 120),
             nn.ReLU(),
         )
-        self.layer4 = nn.Sequential(
+
+        self.layer5 = nn.Sequential(
             nn.Linear(120, 84),
             nn.ReLU(),
             nn.Linear(84, 10),
@@ -42,7 +49,7 @@ class Discriminator(nn.Module):
     def forward(self, x):
         logits = self.layer2(self.layer1(x))
         logits = self.layer4(self.layer3(logits))
-        return logits
+        return self.layer5(logits)
 
 
 def imshow(img):
