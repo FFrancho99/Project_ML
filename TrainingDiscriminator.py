bf10e1be57f82ccfb73021b1eb852ef3c1f2f797
Nedid <44582153+Nedid@users.noreply.github.com>
2023-05-02 11:30:35 +0200

    Import images from imagewoof folder

--- TrainingDiscriminator.py
+++ TrainingDiscriminator.py
@@ -15,10 +15,11 @@ def load_dataset(data_path):
         #transforms.RandomHorizontalFlip(0.5),
         # Random vertical flip
         #transforms.RandomVerticalFlip(0.3),
+        transforms.RandomCrop((128,128)),
         # transform to tensors
         transforms.ToTensor(),
         # Normalize the pixel values (in R, G, and B channels)
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        transforms.Normalize(mean=0.5,std=0.5)
     ])
 
     # Load all of the images, transforming them
@@ -29,13 +30,21 @@ def load_dataset(data_path):
 
     # Split into training (70% and testing (30%) datasets)
     train_size = int(0.7 * len(full_dataset))
-    test_size = len(full_dataset) - train_size
+    val_size = int(0.2 * len(full_dataset))
+    test_size = len(full_dataset) - train_size - val_size
 
     # use torch.utils.data.random_split for training/test split
-    train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
+    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size, test_size])
 
     # define a loader for the training data we can iterate through in 50-image batches
     train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=50,
+        num_workers=0,
+        shuffle=True
+    )
+
+    val_loader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=50,
         num_workers=0,
@@ -50,7 +59,7 @@ def load_dataset(data_path):
         shuffle=False
     )
 
-    return train_loader, test_loader
+    return train_loader, val_loader, test_loader
 
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize to show images correctly
@@ -58,10 +67,10 @@ def imshow(img):
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-train_folder = "archive/tiny-imagenet-200/tiny-imagenet-200/train"
+train_folder = "imagewoof2-160/imagewoof2-160"
 
 # Get the iterative dataloaders for test and training data
-train_loader, test_loader = load_dataset(train_folder)
+train_loader, val_loader, test_loader = load_dataset(train_folder)
 batch_size = train_loader.batch_size
 print("Data loaders ready to read", train_folder)
 
