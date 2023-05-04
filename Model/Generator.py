50b2cf7c6f41f145ab38a3b6e055ecbea8c6b568
Robin <robin.b64@hotmail.com>
2023-05-04 22:21:06 +0200

    Update RobNed with master

--- Model/Generator.py
+++ Model/Generator.py
@@ -12,7 +12,7 @@ from torchvision import transforms
 class GeneratorOptions:
     nC = 3  # rgb
     neF = 64  # nb encoder features
-    nbF = 1000  # nb bottleneck features
+    nbF = 4000  # nb bottleneck features
     ndF = 64  # nb decoder features
 
     def __init__(self):
@@ -21,7 +21,6 @@ class GeneratorOptions:
         self.nbF = 1000  # nb bottleneck features
         self.ndF = 64  # nb decoder features
 
-
 class Generator(nn.Module):
     def __init__(self, mod_opt):
         self.mod_opt = mod_opt
@@ -29,60 +28,61 @@ class Generator(nn.Module):
         self.encoder = nn.Sequential(
             # encoder: conv+AF+pool (pool done with the stride 2 in conv)
             # 128x128 x nC
-            nn.Conv2d(mod_opt.nC, mod_opt.neF, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(mod_opt.nC, mod_opt.neF, kernel_size=(4, 4), stride=2, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
             # 64x64 x neF
-            nn.Conv2d(mod_opt.neF, mod_opt.neF*2, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(mod_opt.neF, mod_opt.neF, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(mod_opt.neF),
             nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
             # 32x32 x neF
-            nn.Conv2d(mod_opt.neF*2, mod_opt.neF * 4, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(mod_opt.neF, mod_opt.neF * 2, 4, 2, 1),
+            nn.BatchNorm2d(mod_opt.neF * 2),
             nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
             # 16x16 x neF*2
-            nn.Conv2d(mod_opt.neF * 4, mod_opt.neF * 8, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(mod_opt.neF * 2, mod_opt.neF * 4, 4, 2, 1),
+            nn.BatchNorm2d(mod_opt.neF * 4),
             nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
             # 8x8 x neF*4
-            nn.Conv2d(mod_opt.neF * 8, mod_opt.neF * 16, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(mod_opt.neF * 4, mod_opt.neF * 8, 4, 2, 1),
+            nn.BatchNorm2d(mod_opt.neF * 8),
             nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=4, stride=4),
-            #nn.Conv2d(mod_opt.neF * 4, mod_opt.neF * 8, 4, 2, 0),
-            #nn.BatchNorm2d(mod_opt.neF * 8),
-            #nn.ReLU(True)
             # 4x4 x neF*8
-
-            #nn.Conv2d(mod_opt.neF * 8, mod_opt.nbF, 4, 1, 0),
-            #nn.BatchNorm2d(mod_opt.nbF),
-            #nn.ReLU(True)
+            nn.Conv2d(mod_opt.neF * 8, mod_opt.nbF, 4, 1, 0),
+            nn.BatchNorm2d(mod_opt.nbF),
+            nn.ReLU(True)
             # 1x1 x nbF
         )
-
 
         self.decoder = nn.Sequential(
             # 1x1 x nbF
+            nn.ConvTranspose2d(mod_opt.nbF, mod_opt.ndF * 8, 4, 1, 0),
+            nn.BatchNorm2d(mod_opt.ndF * 8),
+            nn.ReLU(True),
+            # 4x4 x ndF*8
+            nn.ConvTranspose2d(mod_opt.ndF * 8, mod_opt.ndF * 4, 4, 2, 1),
+            nn.BatchNorm2d(mod_opt.ndF * 4),
+            nn.ReLU(True),
             # 8x8 x ndF*4
-            nn.ConvTranspose2d(mod_opt.ndF * 16, mod_opt.ndF * 8, kernel_size=5, stride=4, padding=1),
+            nn.ConvTranspose2d(mod_opt.ndF * 4, mod_opt.ndF * 2, 4, 2, 1),
+            nn.BatchNorm2d(mod_opt.ndF * 2),
             nn.ReLU(True),
             # 16x16 x ndF*2
-            nn.ConvTranspose2d(mod_opt.ndF * 8, mod_opt.ndF*4, kernel_size=5, stride=2, padding=1),
+            nn.ConvTranspose2d(mod_opt.ndF * 2, mod_opt.ndF, 4, 2, 1),
+            nn.BatchNorm2d(mod_opt.ndF),
             nn.ReLU(True),
             # 32x32 x ndF
-            nn.ConvTranspose2d(mod_opt.ndF*4, mod_opt.ndF*2, kernel_size=5, stride=2, padding=1),
-            nn.ReLU(True),
+            nn.ConvTranspose2d(mod_opt.ndF, mod_opt.nC, 4, 2, 1),
+            nn.BatchNorm2d(mod_opt.nC),
+            # nn.ReLU(True),
             # 64x64 x ndF
-
-            nn.ConvTranspose2d(mod_opt.ndF * 2, mod_opt.nC, kernel_size=4, stride=2, padding=0),
-
-            #nn.ConvTranspose2d(mod_opt.ndF * 2, mod_opt.ndF, kernel_size=5, stride=2, padding=1),
-            #nn.ReLU(True),
-            #nn.ConvTranspose2d(mod_opt.ndF, mod_opt.nC, kernel_size=4, stride=2, padding=0),
+            # nn.ConvTranspose2d(mod_opt.ndF, mod_opt.nC, 4, 2, 1),
+            # nn.BatchNorm2d(mod_opt.nC)
             # 64x64 x nc
         )
+
 
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
         x = torch.tanh(x)
-        return x
+        return
--- Training.py
+++ Training.py
@@ -2,6 +2,8 @@ import torchvision.datasets
 
 from Model import Generator
 from Model import Discriminator
+from TrainingFunctions import imshow
+import TrainingFunctions
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,7 +19,7 @@ from Preprocessing import *
 # ### PREPROCESSING ###
 #
 # imgSize = 128
-batch_size = 64
+
 # trainDataset = loadImages('archive/tiny-imagenet-200/tiny-imagenet-200/train', imgSize, btchSize)
 # trainLoader, trainLoaderCrop = cropPatches(trainDataset, 64, 64)
 # trainLoaderCrop = scalingToOne(trainLoaderCrop)
@@ -36,106 +38,141 @@ batch_size = 64
 # saveImgs(trainLoaderCrop, 1)
 
 ### Load dataset ###
-transform = torchvision.transforms.Compose(
-    [torchvision.transforms.ToTensor(),
-     torchvision.transforms.Normalize((0.5), (0.5))])
+batch_size = 64
+train_folder = "dataset/imagewoof2-160"
+nb_epochs = 10
+learningRate = 0.0001
 
+# Get the iterative dataloaders for test and training data
+train_loader, val_loader, test_loader = TrainingFunctions.load_dataset(train_folder, batch_size)
+print("Data loaders ready to read", train_folder)
 
-trainset = torchvision.datasets.ImageFolder(root='archive/tiny-imagenet-200/tiny-imagenet-200/train', transform=transform)
-valset = torchvision.datasets.ImageFolder(root='archive/tiny-imagenet-200/tiny-imagenet-200/val', transform=transform)
+dataiter = iter(train_loader)
+example_images, _ = next(dataiter)
+print(example_images.shape)
+TrainingFunctions.imshow(torchvision.utils.make_grid(example_images))
 
-print(trainset)
-
-# Create dataloaders
-trainLoader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True)
-valLoader = torch.utils.data.DataLoader(valset, batch_size=batch_size,
-                                        shuffle=False)
-
-
-### Instanciate model  ###
-
+# Instanciate model
+device = 'cuda:0'
 generator_options = Generator.GeneratorOptions
-generator = Generator.Generator(generator_options)
-discriminator = Discriminator.Discriminator(3,64)
+generator = Generator.Generator(generator_options).to(device)
+discriminator = Discriminator.Discriminator(input_channels=3, size=64).to(device)
+real_label = 1
+fake_label = 0
 
 ### Instanciate optimizer  ###
 
-nb_epochs = 1
-batchSize = 64
-learningRate = 0.0001
-
+beta1 = 0.5
 criterion = nn.BCELoss()
-optimizer = optim.Adam(discriminator.parameters(), lr=learningRate)
+optimizerD = optim.Adam(discriminator.parameters(), lr=learningRate, betas=(beta1, 0.999))
+optimizerG = optim.Adam(generator.parameters(), lr=learningRate, betas=(beta1, 0.999))
 
 ### Training ###
-device = 'cpu'
-tr_losses = np.zeros(nb_epochs)
-val_losses = np.zeros(nb_epochs)
+tr_lossesG = np.zeros(nb_epochs)
+tr_lossesD = np.zeros(nb_epochs)
+val_lossesG = np.zeros(nb_epochs)
+val_lossesD = np.zeros(nb_epochs)
+
 
 for epoch_nr in range(nb_epochs):
 
     print("Epoch {}:".format(epoch_nr))
-
     # Train model
-    running_loss = 0.0
-    for batch_im_ori, _ in trainLoader:
+    running_lossD = 0.0
+    running_lossG = 0.0
+    for batch_im_ori, _ in train_loader:
+        ############################################
+        ########### Train Discriminator ############
+        ############################################
+        optimizerD.zero_grad()
 
         # remove (crop) patch from image
-        batch_im_crop, batch_patch_ori = cropPatches(batch_im_ori, 32, 32)
+        batch_im_crop, batch_patch_ori = cropPatches(batch_im_ori, 64, 64)
         # Put data on device
         batch_im_ori = batch_im_ori.to(device)
         batch_im_crop = batch_im_crop.to(device)
+        batch_patch_ori = batch_patch_ori.to(device)
+
+        ## train with real input -> LABEL=1
+        batch_labels = torch.full((batch_im_ori.size(0), 1), real_label, dtype=torch.float, device=device)
+
+        # Predict and get loss
+        predicted_proba = discriminator(batch_patch_ori)
+        lossD_real = criterion(predicted_proba, batch_labels)  # batch_data = label here
+        # compute gradient
+
+        lossD_real.backward(retain_graph=True)
+
+        ## train with fake input -> LABEL=0
+        batch_labels.fill_(fake_label)
 
         # Predict and get loss
         predicted_patch = generator(batch_im_crop)
-        loss = criterion(predicted_patch, batch_patch_ori)  # batch_data = label here
+        predicted_proba = discriminator(predicted_patch)
+        lossD_fake = criterion(predicted_proba, batch_labels)  # batch_data = label here
+        # compute gradient
+        lossD_fake.backward(retain_graph=True)
+
+        # update D
+        lossD = lossD_real + lossD_fake
+        optimizerD.step()
+
+        ############################################
+        ############# Train Generator ##############
+        ############################################
+        optimizerG.zero_grad() # re-initialize the gradient to zero
+        # Predict and get loss
+        batch_labels.fill_(real_label)
+        predicted_proba = discriminator(predicted_patch) # recompute the proba since D has been updated
+        lossG = criterion(predicted_proba, batch_labels)  # batch_data = label here
 
         # Update model
-        optimizer.zero_grad()  # re-initialize the gradient to zero
-        loss.backward()
-        optimizer.step()
+
+        lossG.backward()
+        optimizerG.step()
 
         # Keep running statistics
-        running_loss += loss.item()
+        running_lossD += lossD.item()
+        running_lossG += lossG.item()
 
     # Print results
-    tr_loss = running_loss / len(trainLoader.dataset)
-    print('>> TRAIN: Epoch {} completed | tr_loss: {:.4f}'.format(
-        epoch_nr, running_loss / len(trainLoader.dataset)))
+    tr_lossG = running_lossG / len(train_loader.dataset)
+    tr_lossD = running_lossD / len(train_loader.dataset)
+    print('>> TRAIN: Epoch {} completed | tr_lossG: {:.4f} | tr_lossD: {:.4f}'.format(
+        epoch_nr, tr_lossG, tr_lossD))
 
+    tr_lossesG[epoch_nr] = tr_lossG
+    tr_lossesD[epoch_nr] = tr_lossD
     # Get validation results
     running_loss = 0
 
-    with torch.no_grad():
-        for batch_im_ori, batch_im_crop in valLoader:
-            # Put data on device
-            batch_im_ori = batch_im_ori.to(device)
-            batch_im_crop = batch_im_crop.to(device)
-
-            # Predict and get loss
-            predicted_patch = generator(batch_im_crop)
-            loss = criterion(predicted_patch, batch_im_ori)  # batch_data is the label here
-
-            # Keep running statistics
-            running_loss += criterion(predicted_patch, batch_im_ori)  # batch_data = label
-
-    val_loss = running_loss / len(valLoader.dataset)
-    print('>> VALIDATION: Epoch {} | val_loss: {:.4f}'.format(epoch_nr, val_loss))
-
-    tr_losses[epoch_nr] = tr_loss
-    val_losses[epoch_nr] = val_loss
+    # with torch.no_grad():
+    #     for batch_im_ori, batch_im_crop in val_loader:
+    #         # Put data on device
+    #         batch_im_ori = batch_im_ori.to(device)
+    #         batch_im_crop = batch_im_crop.to(device)
+    #
+    #         # Predict and get loss
+    #         predicted_patch = generator(batch_im_crop)
+    #         loss = criterion(predicted_patch, batch_im_ori)  # batch_data is the label here
+    #
+    #         # Keep running statistics
+    #         running_loss += criterion(predicted_patch, batch_im_ori)  # batch_data = label
+    #
+    # val_loss = running_loss / len(val_loader.dataset)
+    # print('>> VALIDATION: Epoch {} | val_loss: {:.4f}'.format(epoch_nr, val_loss))
+    #
+    #
+    # val_lossesG[epoch_nr] = val_loss
 
 print('Training finished')
+testiter = iter(test_loader)
+real_batch, example_labels = next(testiter)
+batch_im_crop, batch_patch_ori = cropPatches(real_batch, 64, 64)
+predicted_patch = generator(batch_im_crop.to(device))
 
 
-def imshow(img):
-    img = img / 2 + 0.5  # unnormalize to show images correctly
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+# Show images
+imshow(torchvision.utils.make_grid(batch_patch_ori.cpu()))
+imshow(torchvision.utils.make_grid(predicted_patch.cpu().view(batch_patch_ori.shape)))
 
-
-def compute_run_acc(logits, labels):
-    _, pred = torch.max(logits.data, 1)
-    return (pred == labels).sum().item()
